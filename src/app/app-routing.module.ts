fcedc08a6b17e9647cda59964ac52965448a6160
zoblou <zoe.schnetler@gmail.com>
2020-09-14 08:46:58 +0200

    :bug: fixed dashboard bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { CanActivateRouteGuard } from './can-activate-route.guard';
-import { RegisterComponent } from './register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component'
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { CanActivateRouteGuard } from "./can-activate-route.guard";
+import { RegisterComponent } from "./register/register.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'home', 
+  { path: "", component: LoginComponent },
+  {
+    path: "home",
     component: HomeComponent,
-    canActivate: [CanActivateRouteGuard]  
+    canActivate: [CanActivateRouteGuard],
   },
-  { path: 'dashboard', 
-  component: DashboardComponent,
-  canActivate: [CanActivateRouteGuard]  
-},
-  { path: 'register', 
-    component: RegisterComponent }
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    canActivate: [CanActivateRouteGuard],
+  },
+  { path: "register", component: RegisterComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
